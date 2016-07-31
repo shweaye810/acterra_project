76dbf729e3bedea62403ac43a192f82adcef164e
Shwe Aye <shweaye810@gmail.com>
2016-07-31 13:35:21 -0700

    add c_tor(), make form validate and send data to db. if succeed, prompt success; else, prompt error;

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+                $this->load->helper('url_helper');
+		//how to load model into view
+		$this->load->model('model_events'); //load the model into controller
+	}
 	public function index()
 	{
 		$this->getFormDDLData();
@@ -10,10 +17,38 @@ class Welcome extends CI_Controller {
 
 	public function getFormDDLData()
 	{
-		//how to load model into view
-		$this->load->model('model_events'); //load the model into controller
+		/*
+		 * load form helper and library
+		 */
+		$this->load->helper('form');
+                $this->load->library('form_validation');
+
 		$data['location'] = $this->model_events->getLocationList();
 		$data['eventType'] = $this->model_events->getEventType();
-		$this->load->view('welcome_message',$data);
+
+		$this->set_require('location');
+		$this->set_require('event_type');
+		$this->set_require('event_name');
+		$this->set_require('duration');
+
+		if ($this->form_validation->run() === FALSE)
+                {
+                        $this->load->view('welcome_message', $data);
+
+                }
+                else
+                {
+			$this->model_events->set_events_data();
+                        $this->load->view('success');
+                }
+	}
+	private function set_require($name)
+	{
+		$tmp = ucfirst($name);
+		$this->form_validation->set_rules(
+			$name,
+			$tmp,
+			'required'
+		);
 	}
 }
