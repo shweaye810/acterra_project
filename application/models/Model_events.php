e235cd4629bdc9f06f378706aba26373adbbf63a
Shwe Aye <shweaye810@gmail.com>
2016-07-31 13:36:48 -0700

    implements set_events_data() to get data from form page and insert into db

--- application/models/Model_events.php
+++ application/models/Model_events.php
@@ -11,7 +11,8 @@ class model_events extends CI_Model
     }
 
     //query db to get location list for ddl
-    function getLocationList(){
+    function getLocationList()
+    {
         $query =$this->db->query('SELECT * FROM location');
 
         if($query->num_rows()>0){
@@ -22,13 +23,35 @@ class model_events extends CI_Model
     }
 
     //query db to get all event type list for ddl
-    function getEventType(){
+    function getEventType()
+    {
         $query = $this->db->query('SELECT * FROM TypeOfEvent');
         if($query->num_rows()>0){
             return $query->result();
         }else{
             return NULL;
         }
+    }
+    public function set_events_data()
+    {
+        $this->load->helper('url');
+
+        $data = array(
+            'location' => $this->input->post('location'),
+            'event_type' => $this->input->post('event_type'),
+            'event_name' => $this->input->post('event_name'),
+            'organization_name' => $this->input->post('organization_name'),
+            'young' => $this->input->post('young'),
+            'adult' => $this->input->post('adult'),
+            'duration' => $this->input->post('duration'),
+            'area_weeded' => $this->input->post('area_weeded'),
+            'creek_cleared' => $this->input->post('creek_cleared'),
+            'trash_removed' => $this->input->post('trash_removed'),
+            'recycled' => $this->input->post('recycled'),
+            'comments' => $this->input->post('comments'),
+        );
+
+        return $this->db->insert('site_data', $data);
     }
 
 }
